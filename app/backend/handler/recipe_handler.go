f7cf2755e931852c429565d8f82468e780fe4980
Nil Borodulia <nil.b@oxtech.org>
2023-07-28 11:41:36 +0300

    move to handler

--- app/backend/handler/recipe_handler.go
+++ app/backend/handler/recipe_handler.go
@@ -1,21 +1,30 @@
 package handler
 
 import (
+    "io"
+    "fmt"
     "net/http"
+    "database/sql"
+    "strconv"
     "github.com/go-chi/chi/v5"
-    repository "go-cooking-recipes/v1/app/backend/repository"
+    "github.com/go-chi/render"
+    recipe "go-cooking-recipes/v1/app/backend/repository"
+    "encoding/json"
+    "github.com/google/uuid"
 )
 
+type JSON map[string]interface{}
+
 type Handler struct {
-    Repository repository.Repository
+    Connection *sql.DB
 }
 
-func NewHandler() Handler {
-    return Handler{}
+func NewHandler(conn *sql.DB) Handler {
+    return Handler{Connection: conn}
 }
 
 
-func (h Handler) onListRecipe(w http.ResponseWriter, r *http.Request) {
+func (h Handler) OnListRecipe(w http.ResponseWriter, r *http.Request) {
     limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
     order := r.URL.Query().Get("order")
     page, _ := strconv.Atoi(r.URL.Query().Get("page"))
@@ -25,7 +34,7 @@ func (h Handler) onListRecipe(w http.ResponseWriter, r *http.Request) {
         Order: order,
         Page: page,
      }
-    recipeRepository := recipe.NewRecipeRepository(h.Repository.Connection)
+    recipeRepository := recipe.NewRecipeRepository(h.Connection)
     listData, err := recipeRepository.GetList(param)
 
     if err != nil {
@@ -36,3 +45,102 @@ func (h Handler) onListRecipe(w http.ResponseWriter, r *http.Request) {
 
     render.JSON(w, r, JSON{"status": "ok", "data": listData})
 }
+
+func (h Handler) OnGetOneRecipe(w http.ResponseWriter, r *http.Request) {
+    uuid := chi.URLParam(r, "uuid")
+
+    recipeRepository := recipe.NewRecipeRepository(h.Connection)
+    row, err := recipeRepository.GetOne(uuid)
+
+    if err != nil {
+         render.Status(r, http.StatusNotFound)
+         render.JSON(w, r, JSON{"status": "error"})
+         return
+    }
+
+    render.JSON(w, r, JSON{"status": "ok", "data": row})
+}
+
+func (h Handler) OnDeleteOneRecipe(w http.ResponseWriter, r *http.Request) {
+    uuid := chi.URLParam(r, "uuid")
+
+    recipeRepository := recipe.NewRecipeRepository(h.Connection)
+    err := recipeRepository.Remove(uuid)
+    if err != nil {
+         render.Status(r, http.StatusNotFound)
+         render.JSON(w, r, JSON{"status": "error"})
+         return
+    }
+    render.Status(r, http.StatusNoContent)
+    render.JSON(w, r, JSON{"status": "deleted"})
+}
+
+func (h Handler) OnCreateRecipe(w http.ResponseWriter, r *http.Request) {
+
+    var recipeData JSON
+
+    b, err := io.ReadAll(r.Body)
+    if err != nil {
+        fmt.Printf("[ERROR] %s", err)
+    }
+
+    err = json.Unmarshal(b, &recipeData)
+
+     if err != nil {
+        fmt.Println("Error while decoding the data", err.Error())
+     }
+     uuid := uuid.New().String()
+
+     rec := recipe.Recipe{
+         Uuid: uuid,
+         Name: recipeData["name"].(string),
+         Description: recipeData["description"].(string),
+         Text: recipeData["text"].(string),
+         Image: recipeData["image"].(string),
+         Labels: recipeData["labels"].(string),
+     }
+     recipeRepository := recipe.NewRecipeRepository(h.Connection)
+     err = recipeRepository.Create(rec)
+     if err != nil {
+        render.Status(r, http.StatusBadRequest)
+        render.JSON(w, r, JSON{"status": "error", "message": err})
+     }
+
+     render.Status(r, http.StatusCreated)
+     render.JSON(w, r, JSON{"status": "ok", "uuid": uuid})
+}
+
+func (h Handler) OnChangeOneRecipe(w http.ResponseWriter, r *http.Request) {
+    uuid := chi.URLParam(r, "uuid")
+
+    var recipeData JSON
+
+    b, err := io.ReadAll(r.Body)
+    if err != nil {
+        fmt.Printf("[ERROR] %s", err)
+    }
+
+    err = json.Unmarshal(b, &recipeData)
+
+     if err != nil {
+        fmt.Println("Error while decoding the data", err.Error())
+     }
+
+     rec := recipe.Recipe{
+         Name: recipeData["name"].(string),
+         Description: recipeData["description"].(string),
+         Text: recipeData["text"].(string),
+         Image: recipeData["image"].(string),
+         Labels: recipeData["labels"].(string),
+     }
+     recipeRepository := recipe.NewRecipeRepository(h.Connection)
+     _, err = recipeRepository.Change(uuid, rec)
+     if err != nil {
+        render.Status(r, http.StatusBadRequest)
+        render.JSON(w, r, JSON{"status": "error", "message": err})
+     }
+
+     render.Status(r, http.StatusOK)
+     render.JSON(w, r, JSON{"status": "ok", "uuid": uuid})
+}
+
